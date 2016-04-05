1cc4559b4f1d614823b1e2da6332a5de39835bc6
agustinchavez <chavez13@illinois.edu>
2016-04-05 18:33:25 -0500

    added icons and user locations

--- app/controllers/pitches_controller.rb
+++ app/controllers/pitches_controller.rb
@@ -2,6 +2,8 @@ class PitchesController < ApplicationController
 
     include ParserHelper
 
+    PITCHES ||= ParserHelper.get_data
+
   def index
     @pitches = Pitch.all
     render "/pitches/index"
@@ -10,6 +12,5 @@ class PitchesController < ApplicationController
   def user_latitude_longitude
     @user_location = Geokit::LatLng.new(params[:lat].to_f, params[:lng].to_f)
     @pitch = Pitch.closest(:origin => @user_location).first
-    binding.pry
   end
 end
