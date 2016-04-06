436bca51c747ed757ac2c93437d770a48708061f
agustinchavez <chavez13@illinois.edu>
2016-04-06 11:05:34 -0500

    fixes made to street view functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
   root 'pitches#index'
+
   resources :pitches, only: [:index, :show]
     post 'pitches/user_latitude_longitude', :to => 'pitches#user_latitude_longitude'
+
 end
