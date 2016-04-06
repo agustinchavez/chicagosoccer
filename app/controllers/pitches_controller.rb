944ae50664cc9a39ceefd58087e9ebf301c3c022
agustinchavez <chavez13@illinois.edu>
2016-04-06 11:27:49 -0500

    added streetview.js file

--- app/controllers/pitches_controller.rb
+++ app/controllers/pitches_controller.rb
@@ -12,7 +12,7 @@ class PitchesController < ApplicationController
   def user_latitude_longitude
     @user_location = Geokit::LatLng.new(params[:lat].to_f, params[:lng].to_f)
     @pitch = Pitch.closest(:origin => @user_location).first
-    binding.pry
+
   end
 
   def coordinate_conversion
