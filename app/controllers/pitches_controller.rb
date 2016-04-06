73516c6696529fd56dfa89ed0500cb80acd9b5bf
agustinchavez <chavez13@illinois.edu>
2016-04-06 12:03:57 -0500

    driving directions setup

--- app/controllers/pitches_controller.rb
+++ app/controllers/pitches_controller.rb
@@ -8,7 +8,7 @@ class PitchesController < ApplicationController
   def user_latitude_longitude
     @user_location = Geokit::LatLng.new(params[:lat].to_f, params[:lng].to_f)
     @pitch = Pitch.closest(:origin => @user_location).first
-
+    render "/pitches/driving_directions"
   end
 
   def coordinate_conversion
